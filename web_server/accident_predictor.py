b831d3b6e2542799863a62f7607ff23e8d9109a4
Pedro Perez Sanchez <pedropobla@gmail.com>
2015-12-13 03:05:45 -0500

    Show CartoDB map

--- web_server/accident_predictor.py
+++ web_server/accident_predictor.py
@@ -1,16 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 from weather_api import WeatherApi
 import datetime
+import os
 
 app = Flask(__name__)
+app.config['DEBUG'] = True if os.environ.get("FLASK_DEBUG") else False
 
 @app.route("/")
 def index():
   today = datetime.datetime.now()
-  return ("weekday: " + today.strftime("%A") + " (" + str(today.weekday()) + ")</br>" +
-          "month: " + today.strftime("%B") + " (" + str(today.month) + ")</br>" +
-          "weather: " + WeatherApi.current_weather_condition())
+  map_input = {
+    "day": today.strftime("%A"),
+    "conditions": WeatherApi.current_weather_condition()
+  }
+  return render_template("map.html", map_input = map_input, title = "Map")
 
 if __name__ == "__main__":
-  app.debug = True
   app.run()
